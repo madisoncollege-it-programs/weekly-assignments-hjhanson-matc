c2175fdcd14d2115f1f79d8176f3e81ad298da20
Hunter Hanson <hjhanson@madisoncollege.edu>
2022-01-27 12:56:00 -0600

    READY FOR GRADING - Adding py script for assignment submission

---
+++ week2/first-script.py
@@ -0,0 +1,3 @@
+print ("Hunter Hanson")
+print ("I learned that with python you don't need to surround the code with elements like in HTML,")
+print ("which is the only code language I have experience with.")
