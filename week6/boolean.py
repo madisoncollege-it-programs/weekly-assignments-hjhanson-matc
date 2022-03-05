ca4bfd920d0e2393a0fe5c42cfa1c59439dcf44f
Hunter Hanson <hjhanson@madisoncollege.edu>
2022-03-05 15:00:36 -0600

    READY FOR GRADING - Boolean Logic Basics Lab

---
+++ week6/boolean.py
@@ -0,0 +1,77 @@
+#Hunter Hanson
+
+#1
+print ("True and True is:")
+print (bool(True and True))
+print ()
+#2
+print ("False and True is:")
+print (bool(False and True))
+print ()
+#3
+print ("1 == 1 and 2 == 1 is:")
+print (bool(1 == 1 and 2 == 1))
+print ()
+#4
+print ("0 is:")
+print (bool(0))
+print ()
+#5
+print ("\"\" is: ")
+print (bool(""))
+print ()
+#6
+print ("0.0 is:")
+print (bool(0.0))
+print ()
+#7
+print ("not 0 is:")
+print (bool(not 0))
+print ()
+#8
+print ("\'test\' == \'test\' is:")
+print (bool('test' == 'test'))
+print ()
+#9
+print ("1 == 1 or 2 != 1 is:")
+print (bool(1 == 1 or 2 != 1))
+print ()
+#10
+print ("True and 1 == 1 is: ")
+print (bool(True and 1 == 1))
+print ()
+#11
+print ("False and 0 != 0 is:")
+print (bool(False and 0 != 0))
+print ()
+#12
+print ("True or 1 == 1 is:")
+print (bool(True or 1 == 1))
+print ()
+#13
+print ("\'test\' == \'testing\' is:")
+print (bool('test' == 'testing'))
+print ()
+#14
+print ("1 != 0 and 2 == 1 is:")
+print (bool(1 != 0 and 2 == 1))
+print ()
+#15
+print ("\'test\' != \'testing\' is:")
+print (bool('test' != 'testing'))
+print ()
+#16
+print ("\'test\' == 1 is:")
+print (bool('test' == 1))
+print ()
+#17
+print ("1 == 1 and not (\'testing\' == 1 or 1 == 0) is:")
+print (bool(1 == 1 and not ('testing' == 1 or 1 == 0)))
+print ()
+#18
+print ("\'chunky\' == \'bacon\' and not (3 == 4 or 3 == 3) is:")
+print (bool('chunky' == 'bacon' and not (3 == 4 or 3 == 3)))
+print ()
+#19
+print ("3 == 3 and not (\'testing\' == 1 or \'Python\' == \'Fun\') is:")
+print (bool(3 == 3 and not ('testing' == 1 or 'Python' == 'Fun')))
