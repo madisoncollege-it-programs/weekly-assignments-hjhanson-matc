adfab1eafffe82feeacfa283226395f65dae884c
Hunter Hanson <hjhanson@madisoncollege.edu>
2022-03-05 14:25:42 -0600

    WIP - File Data Interaction Lab

--- week5/files.py
+++ week5/files.py
@@ -1,18 +1,37 @@
 #Hunter Hanson
 #This script copys the passwd file to a new file.
 
-hFile = open("/etc/passwd","r")
+#Question 1
+#aFile = open("/etc/passwd",'r')
 
-indata = hFile.read()
+#indata = aFile.read()
 
-print (f"The passwd file is {len(indata)} bytes long")
-print ("This length includes all unicode characters, including line breaks.")
+#to_file = ("passwd_str.txt")
+#out_file = open(to_file, 'w')
+#out_file.write(indata)
+#out_file.close()
+
+#bFile = open("passwd_str.txt",'r')
+#strPasswd = bFile.read()
+
+#print (f"The copied file is {len(strPasswd)} bytes long")
+#print ("This length includes all unicode characters, including line breaks.")
+#print ("You would use this read function over others when you want to work with entire documents,")
+#print ("it works best with short documents.")
 
 
-to_file = ("passwd_str.txt")
+#bFile.close()
+#aFile.close()
 
+
+#Question 2
+aFile = open("/etc/passwd", 'r')
+
+indata = aFile.readlines()
+
+to_file = ("passwd_list.txt")
 out_file = open(to_file, 'w')
-out_file.write(indata)
-
+out_file.writelines(indata)
 out_file.close()
-hFile.close()
+
+aFile.close()
